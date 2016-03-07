8ab91880c65dd1fad0eb4e9ddcb201d420210fff
Lovinder Pnag <lspnag@gmail.com>
2016-03-06 21:02:49 -0500

    added TreeMap Hash

--- src/RomanNumeral.java
+++ src/RomanNumeral.java
@@ -1,13 +1,26 @@
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 public class RomanNumeral {
 
     private final int arabic;
+    private NavigableMap<Integer, String> navMap;
 
     public RomanNumeral(int arabic) {
         this.arabic = arabic;
+
+//  The outer pair of braces means that you are declaring and initializing an anonymous inner class that extends TreeMap. The inner pair of braces represents an instance initializer, code that is run when an instance is created.
+
+        navMap = new TreeMap<Integer, String>() {
+            {
+                put(1, "I");
+            }
+        };
+
     }
 
     public String convertArabic() {
+        System.out.println(navMap);
         if (arabic == 1) {
             return "I";
         } else {
--- test/convertArabicTest.java
+++ test/convertArabicTest.java
@@ -4,15 +4,27 @@ import static junit.framework.TestCase.assertEquals;
 
 public class convertArabicTest {
 
+    private RomanNumeral number;
+
+    private void setArabic(int num) {
+        number = new RomanNumeral(num);
+    }
+
     @Test
     public void convertArabicIsPassedOneAndReturnsNumeralI() {
-        RomanNumeral number = new RomanNumeral(1);
+        setArabic(1);
         assertEquals("I", number.convertArabic());
     }
 
     @Test
     public void convertArabicIsPassedTwoAndReturnsNumeralII() {
-        RomanNumeral number = new RomanNumeral(2);
+        setArabic(2);
         assertEquals("II", number.convertArabic());
+    }
+
+    @Test
+    public void convertArabicIsPassedThreeAndReturnsAppendedStringIII() {
+        setArabic(3);
+        assertEquals("III", number.convertArabic());
     }
 }
