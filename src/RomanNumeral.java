841e84245e00c9e860a79583560bab1ab975eda8
Lovinder Pnag <lspnag@gmail.com>
2016-03-06 20:44:06 -0500

    passing first test and failing second

---
+++ src/RomanNumeral.java
@@ -0,0 +1,13 @@
+
+public class RomanNumeral {
+
+    private final int arabic;
+
+    public RomanNumeral(int arabic) {
+        this.arabic = arabic;
+    }
+
+    public String convertArabic() {
+        return "I";
+    }
+}
--- test/convertArabicTest.java
+++ test/convertArabicTest.java
@@ -1,10 +1,18 @@
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class convertArabicTest {
 
     @Test
     public void convertArabicIsPassedOneAndReturnsNumeralI() {
         RomanNumeral number = new RomanNumeral(1);
         assertEquals("I", number.convertArabic());
+    }
+
+    @Test
+    public void convertArabicIsPassedTwoAndReturnsNumeralII() {
+        RomanNumeral number = new RomanNumeral(2);
+        assertEquals("II", number.convertArabic());
     }
 }
